8e2af9f3fd0e737e9d472f532ea50e92d566d0cf
AlanGomez2022 <alanagustingomez@gmail.com>
2022-10-05 23:45:39 -0300

    4to Commit
    Se termino la pagina comprobante

---
+++ js/comprobanteApp.js
@@ -0,0 +1,97 @@
+let reloj = document.getElementById ("reloj")
+const diaSemana = document.createElement("p")
+const hora = document.createElement("p")
+//primero me guardo la fecha y la hora del comprobante
+fetch("http://worldtimeapi.org/api//America/Argentina/Buenos_Aires")
+.then((res) => res.json())
+.then ((resultado) =>{
+  let fecha="";
+  let dia=[0,"Lunes","Martes","Miercoles", "Jueves","Viernes","Sabado","Domingo"];
+  for (let i=0;i<10;i++ ) {
+    fecha+=resultado.datetime[i];
+  }
+
+  diaSemana.innerHTML=`
+  <p>${dia[resultado.day_of_week]}  ${fecha}</p>
+  `
+  let time="";
+  for (let i=11;i<19;i++){
+    time+=resultado.datetime[i];
+
+  }
+  hora.innerHTML=`
+    <p>Horario: ${time}</p>
+  `
+
+  reloj.append(diaSemana)
+  reloj.append(hora)
+})
+//me traigo desde el LOCALSTORAGE todos los valores que voy a mosrtar en el comprobante
+const compraTicket = JSON.parse(localStorage.getItem("compraTicket"))
+const datosPago = JSON.parse(localStorage.getItem("datosPago"))
+
+//hago el destructiring para trabajar mas comodo con variables
+const {personas,sector,precioSector,descuento,costosAdmin,total} = compraTicket;
+const {mail,nameTitularTarjeta,numberCreditCard,tipoTarjeta} = datosPago;
+
+
+let correo = document.getElementById("mail")
+correo.innerText = `${mail} (es el correo registrado)`
+
+
+let empresa = document.getElementById("tipoTarjeta")
+//reviso empresa de la tarjeta
+switch (tipoTarjeta) {
+    case "1":
+        empresa.innerText=`Tarjeta de tipo: VISA`
+    break;
+    case "2":
+        empresa.innerText=`Tarjeta de tipo: MASTER CARD`
+    break;
+    case "3":
+        empresa.innerText=`Tarjeta de tipo: AMERICAN EXPRESS`
+    break;
+
+    default:
+        break;
+}
+let nroCard = document.getElementById("nroCard")
+let elemento = "";
+//obtengo solo los ultimos 4 numeros de la tarjeta
+for (let index = 12; index < numberCreditCard.length; index++) {
+    elemento += numberCreditCard[index]; 
+}
+
+nroCard.innerText=` XXXX XXXX XXXX ${elemento}`
+
+let cantidad = document.getElementById("cantidad")
+
+cantidad.innerText=`${personas.length} -  personas asistiran al evento `
+
+let lugar = document.getElementById("lugar")
+lugar.innerText= `${sector} - Precio $ ${precioSector}`
+
+//aca voy obteniendo un texto donde acumulo cada persona
+//del array en un grupo
+let people = document.getElementById("personas")
+let grupo="";
+let contador=1;
+personas.forEach(element => {
+    grupo += `${contador} - Nombre: ${element.nombre} - \nDNI: ${element.dni}\n`
+    contador++
+});
+people.innerHTML=`<p class="texto">  ${grupo}<p>`
+
+// saco subtotal
+//y finalmente digo si hay descuento
+//y muestro el total de la compra
+
+let subtotal = document.getElementById("subtotal")
+subtotal.innerText=`Subtotal de la compra - $ ${precioSector*personas.length}`
+
+let totalCompra = document.getElementById("total")
+if (descuento==0){
+    totalCompra.innerText=`TOTAL: $ ${total} (Sin descuentos aplicados) `
+}else{
+    totalCompra.innerText=`TOTAL: $ ${total} con descuento Club personal(mostrar Club Personal en la entrada)`
+}
--- js/pagoApp.js
+++ js/pagoApp.js
@@ -37,11 +37,15 @@ selectEmpresa.onchange = () =>{
     
 }
 
+
+
+
+
 //--------------------------------------------------------------------------------------
 // RELOJ CON FECHA QUE UTILIZA UNA API
+//--------------------------------------------------------------------------------------
 
 const tiempo = document.getElementById("reloj")
-const date = document.createElement("p")
 const diaSemana = document.createElement("p")
 const hora = document.createElement("p")
 setInterval(() => {
@@ -66,10 +70,61 @@ setInterval(() => {
         <p>Horario: ${time}</p>
       `
       
-      tiempo.append(date)
+   
       tiempo.append(diaSemana)
       tiempo.append(hora)
     })
 },1000)
 
 
+let form = document.getElementById("form");
+
+const pago = {
+  mail:"",
+  numberCreditCard:0,
+  nameTitularTarjeta:"",
+  tipoTarjeta:"" 
+  //dejo fuera el resto de los datos como vencimiento y CVV
+  //para una futura validacion
+}
+form.addEventListener ("submit",datosForm)
+
+function datosForm (e){
+  e.preventDefault();
+  let datos = e.target
+  pago.mail=datos.children[0].children[1].value;//saco el nombre del mail
+  pago.numberCreditCard=datos.children[1].children[1].value;//el numero de la tarjeta
+  pago.nameTitularTarjeta=datos.children[2].children[1].value;//nombre del titular de la tarjeta
+  //DEJO TODOS ESTOS DATOS COMENTADOS PORQUE NO LOS NECESITO
+  //QUIZAS PARA UNA FUTURA VALIDACION DE TARJETAS CON API DE TARJETAS DE CREDITO
+  //datos.children[3].children[1].children[0].value; //mes vencimiento
+  //datos.children[3].children[1].children[1].value); //año vencimiento
+  //datos.children[4].children[1].children[0].value); //CVV de la tarjeta
+  pago.tipoTarjeta=datos.children[4].children[1].children[1].value;//empresa tarjeta
+
+  localStorage.setItem("datosPago",JSON.stringify(pago));
+  swal({
+    title:"Esta por debitarse el pago por la reserva del evento, ¿Desea continuar?",
+    icon:"info",
+    buttons:{
+        cancel:"No, Debo revisar datos nuevamente",
+        continuar:"SI, deseo continuar",
+    }
+  })
+  .then((value) => {
+    switch (value) {
+   
+        case "continuar":
+            swal(window.location="../pages/comprobante.html");
+            break;
+        
+        case "cancel":
+        swal("continuando");
+        break;
+
+        default:
+        swal("Por favor vuelve a revisar los datos de la compra");
+        break;
+    }
+  });    
+}
